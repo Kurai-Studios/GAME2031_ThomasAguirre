66c54cea2857010c020af1d99aec106feaa1575c
Kurai <fxt.kurai@gmail.com>
2026-03-15 19:36:44 -0400

    Movement system
    Added the starting movement system for the player to move around the platform

--- GAME2031_ThomasAguirre/Assets/_Scripts/PlayerController.cs
+++ GAME2031_ThomasAguirre/Assets/_Scripts/PlayerController.cs
@@ -1,16 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb;
+    private SpriteRenderer playerSprite;
+
+    [Header("Move Settings")]
+    [SerializeField] private float moveForce;
+    [SerializeField] private float maxSpeed;
+
+    private float input;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        input = Input.GetAxisRaw("Horizontal");
+
+        if (input != 0)
+        {
+            playerSprite.flipX = input < 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(rb.linearVelocityX) <= maxSpeed)
+        {
+            rb.AddForceX(input * moveForce);
+        }
+        else
+        {
+            if (Mathf.Sign(input) != Mathf.Sign(rb.linearVelocityX))
+            {
+                rb.AddForceX(input * moveForce);
+            }
+        }
     }
 }
